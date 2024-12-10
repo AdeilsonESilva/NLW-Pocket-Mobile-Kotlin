c702c439462ab698c1469ea70b12b57bc89e003a
Adeilson Silva <tbh.adeilson.silva@sympla.com.br>
2024-12-10 16:20:08 -0300

    Create components and mocks

---
+++ app/src/main/java/br/com/adeilson/nearby/data/model/mock/MockCategories.kt
@@ -0,0 +1,10 @@
+package br.com.adeilson.nearby.data.model.mock
+
+import br.com.adeilson.nearby.data.model.Category
+
+val mockCategories = listOf(
+    Category(id = "1", name = "Alimentação"),
+    Category(id = "2", name = "Cinema"),
+    Category(id = "3", name = "Farmácia"),
+    Category(id = "4", name = "Supermercado")
+)
