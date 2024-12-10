e68f1716f8b11486aeb91b72f424e11ef51a9aba
Adeilson Silva <tbh.adeilson.silva@sympla.com.br>
2024-12-10 11:26:14 -0300

    Update lib version and setup project

---
+++ app/src/main/java/br/com/adeilson/nearby/ui/components/NearbyButton.kt
@@ -0,0 +1,23 @@
+package br.com.adeilson.nearby.ui.components
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NearbyButton(modifier: Modifier = Modifier,
+                 text: String? = null,
+                 @DrawableRes iconRes: Int? = null,
+                 onClick: () -> Unit,
+) {
+    Button(modifier = modifier, onClick = onClick) {}
+}
+
+@Preview
+@Composable
+private fun NewButtonPreview() {
+    NearbyButton(modifier = Modifier.fillMaxWidth(), text="Confirmar") {  }
+}
